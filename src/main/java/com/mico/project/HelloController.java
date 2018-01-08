0ef9f3c5975e1246ff0b9daf5c255f681fe27000
cho <degan8535@gmail.com>
2018-01-08 16:28:51 +0900

    Set spring boot & vue

---
+++ src/main/java/com/mico/project/HelloController.java
@@ -0,0 +1,19 @@
+package com.mico.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller 
+@RequestMapping("/hello") 
+public class HelloController { 
+	
+	@RequestMapping(value="/", method=RequestMethod.GET) 
+	public String hello() { return "hello"; } 
+
+	
+	@RequestMapping("/greeting") 
+	public String greeting() { return "index"; }
+
+}
+
---
+++ src/test/java/com/mico/project/MicoProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mico.project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicoProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
