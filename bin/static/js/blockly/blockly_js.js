1e843b0fc2bd23ac765879300537315d17a7cbee
cho <degan8535@gmail.com>
2018-01-08 18:55:36 +0900

    Add animate blockly and img

---
+++ bin/static/js/blockly/blockly_js.js
@@ -0,0 +1,243 @@
+var blocklyArea = document.getElementById('blocklyArea');
+var blocklyDiv = document.getElementById('blocklyDiv');
+/*var workspace = Blockly.inject(blocklyDiv, options);
+var toolbox = document.getElementById("toolbox");*/
+
+var workspace = Blockly.inject(blocklyDiv,
+    {media: '/media/',
+        toolbox: document.getElementById('toolbox'),
+        zoom:
+            {controls: true,
+                wheel: true,
+                startScale: 1.0,
+                maxScale: 3,
+                minScale: 0.3,
+                scaleSpeed: 1.2},
+        grid:
+            {spacing: 20,
+                length: 2,
+                colour: '#6a74cc',
+                snap: true},
+        horizontalLayout : true,
+        trashcan: true});
+/*collapse : true,
+    comments : true,
+    disable : true,
+    maxBlocks : Infinity,*/
+
+var onresize = function(e) {
+    // Compute the absolute coordinates and dimensions of blocklyArea.
+    var element = blocklyArea;
+    var x = 0;
+    var y = 0;
+    do {
+        x += element.offsetLeft;
+        y += element.offsetTop;
+        element = element.offsetParent;
+    } while (element);
+    // Position blocklyDiv over blocklyArea.
+    blocklyDiv.style.left = x + 'px';
+    blocklyDiv.style.top = y + 'px';
+    blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
+    blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
+};
+
+window.addEventListener('resize', onresize, false);
+onresize();
+Blockly.svgResize(workspace);
+
+function showCode() {
+    // Generate JavaScript code and display it.
+    Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+    var code = Blockly.JavaScript.workspaceToCode(workspace);
+    alert(code);
+}
+
+function runCode() {
+    // Generate JavaScript code and run it.
+    window.LoopTrap = 1000;
+    Blockly.JavaScript.INFINITE_LOOP_TRAP =
+        'if (--window.LoopTrap == 0) throw "Infinite loop.";\n';
+    var code = Blockly.JavaScript.workspaceToCode(workspace);
+    Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+    try {
+        eval(code);
+    } catch (e) {
+        alert(e);
+    }
+}
+
+Blockly.Blocks['direction_block_top'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldDropdown([["up","up"], ["down","down"]]), "direction")
+            .appendField("방향으로")
+            .appendField(new Blockly.FieldNumber(0, 0, 6), "distance")
+            .appendField("이동");
+        this.appendValueInput("easing_function")
+            .setCheck("effect")
+            .setAlign(Blockly.ALIGN_RIGHT)
+            .appendField("효과");
+        this.setPreviousStatement(true, "direction_block_left");
+        this.setNextStatement(true, "direction_block_left");
+        this.setColour(195);
+        this.setTooltip('');
+        this.setHelpUrl('');
+    }
+};
+
+Blockly.JavaScript['direction_block_top'] = function(block) {
+    var direction = block.getFieldValue('direction');
+    var number_distance = block.getFieldValue('distance');
+    var easing_function = Blockly.JavaScript.valueToCode(block, 'easing_function', Blockly.JavaScript.ORDER_NONE);
+
+    distance = number_distance * block_height_size;
+
+    var code ='';
+    var callback_fun = 'function(){check_running_object_position($'+block.getSurroundParent().getFieldValue("running_object_pic")+')})';
+
+    if(easing_function === undefined || easing_function =='') {
+        if(direction === 'up') {
+            code = '.animate({top:"-='+distance+'"}, 1000, ';
+        }else if(direction === 'down') {
+            code = '.animate({top:"+='+distance+'"}, 1000, ';
+        }
+    }else {
+        if(direction === 'up') {
+            code = '.animate({top:"-='+distance+'"}, 1000,"'+easing_function+'", ';
+        }else if(direction === 'down') {
+            code = '.animate({top:"+='+distance+'"}, 1000,"'+easing_function+'", ';
+        }
+    }
+    return code + callback_fun+"\n";
+};
+
+Blockly.Blocks['direction_block_left'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldDropdown([["right","right"], ["left","left"]]), "direction")
+            .appendField("방향으로")
+            .appendField(new Blockly.FieldNumber(0, 0, 12), "distance")
+            .appendField("이동");
+        this.appendValueInput("easing_function")
+            .setCheck("effect")
+            .setAlign(Blockly.ALIGN_RIGHT)
+            .appendField("효과");
+        this.setInputsInline(false);
+        this.setPreviousStatement(true, ["direction_block_left", "direction_block_top"]);
+        this.setNextStatement(true, ["direction_block_left", "direction_block_top"]);
+        this.setColour(180);
+        this.setTooltip('');
+        this.setHelpUrl('');
+    }
+};
+
+Blockly.JavaScript['direction_block_left'] = function(block) {
+    var direction = block.getFieldValue('direction');
+    var number_distance = block.getFieldValue('distance');
+    var easing_function = Blockly.JavaScript.valueToCode(block, 'easing_function', Blockly.JavaScript.ORDER_NONE);
+
+    distance = number_distance * block_width_size;
+
+    var code ='';
+    var callback_fun = 'function(){check_running_object_position($'+block.getSurroundParent().getFieldValue("running_object_pic")+')})';
+
+    if(easing_function === undefined || easing_function =='') {
+        if(direction === 'left') {
+            code = '.animate({left:"-='+distance+'"}, 1000, ';
+        }else if(direction === 'right') {
+            code = '.animate({left:"+='+distance+'"}, 1000, ';
+        }
+    }else {
+        if(direction === 'left') {
+            code = '.animate({left:"-='+distance+'"}, 1000,"'+easing_function+'", ';
+        }else if(direction === 'right') {
+            code = '.animate({left:"+='+distance+'"}, 1000,"'+easing_function+'", ';
+        }
+    }
+
+    return code + callback_fun+"\n";
+};
+
+
+Blockly.Blocks['running_object'] = {
+    init: function() {
+        this.appendStatementInput("running_blocks")
+            .setCheck(["direction_block_left", "direction_block_top"])
+            .appendField(new Blockly.FieldDropdown([[{"src":"/img/girl.png","width":50,"height":50,"alt":"girl"},"girl"],[{"src":"/img/cloud.png","width":50,"height":50,"alt":"cloud"},"cloud"]]), "running_object_pic");
+        this.setInputsInline(true);
+        this.setPreviousStatement(true, null);
+        /*this.setNextStatement(true, null);*/
+        this.setColour(270);
+        this.setTooltip('');
+        this.setHelpUrl('');
+    }
+};
+
+Blockly.JavaScript['running_object'] = function(block) {
+    var dropdown_running_object_pic = block.getFieldValue('running_object_pic');
+    var move_statements = Blockly.JavaScript.statementToCode(block, 'running_blocks');
+
+    var code = '$'+ dropdown_running_object_pic + move_statements;
+
+    return code;
+};
+
+Blockly.Blocks['effects'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldDropdown([["답답이","easeInBounce"], ["팅팅볼","easeInOutBack"], ["머뭇머뭇","easeInOutBounce"]]), "easing");
+        this.setOutput(true, null);
+        this.setColour(330);
+        this.setTooltip('');
+        this.setHelpUrl('');
+    }
+};
+
+Blockly.JavaScript['effects'] = function(block) {
+    var dropdown_easing = block.getFieldValue('easing');
+
+    return [dropdown_easing, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.Blocks['angle_move'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldAngle(90), "move_angle")
+            .appendField("방향으로")
+            .appendField(new Blockly.FieldNumber(0, 0, 20), "move_angle_distance")
+            .appendField("이동");
+        this.appendValueInput("easing_function")
+            .setCheck("effect")
+            .setAlign(Blockly.ALIGN_RIGHT)
+            .appendField("효과");
+        this.setInputsInline(false);
+        this.setPreviousStatement(true, ["direction_block_left", "direction_block_top"]);
+        this.setNextStatement(true, ["direction_block_left", "direction_block_top"]);
+        this.setColour(165);
+        this.setTooltip('');
+        this.setHelpUrl('');
+    }
+};
+
+Blockly.JavaScript['angle_move'] = function(block) {
+    var angle_move_angle = block.getFieldValue('move_angle');
+    var number_move_angle_distance = block.getFieldValue('move_angle_distance');
+    var easing_function = Blockly.JavaScript.valueToCode(block, 'easing_function', Blockly.JavaScript.ORDER_NONE);
+
+    distance = number_move_angle_distance * block_width_size;
+
+    var code ='';
+    var callback_fun = 'function(){check_running_object_position($'+block.getSurroundParent().getFieldValue("running_object_pic")+')})';
+
+    var distance_x = distance * Math.cos(toRadians(angle_move_angle));
+    var distance_y = distance * Math.sin(toRadians(angle_move_angle));
+
+    if(easing_function === undefined || easing_function =='') {
+        code = '.animate({left:"+='+distance_x+'", top:"-='+distance_y+'"}, 1000, ';
+    }else {
+        code = '.animate({left:"+='+distance_x+'", top:"-='+distance_y+'"}, 1000, "'+easing_function+'", ';
+    }
+
+    return code + callback_fun+"\n";
+};
---
+++ bin/static/js/blockly/testAnimateJs3.js
@@ -0,0 +1,171 @@
+/**
+ * save list.
+ */
+var activityData = function(){
+    var saved_list = {};
+
+    // private method
+
+    // public method
+    var _getData = function(key){
+        if(key === undefined){
+            return saved_list;
+        }
+        else{
+            return saved_list[key];
+        }
+    };
+
+    var _setData = function(key, val) {
+        if(key === undefined && val === undefined) {
+            saved_list[story] = [$running_object, started_position, written_text];
+        }else if(val === undefined){
+            saved_list = key;
+        }
+        else{
+            saved_list[key] = val;
+        }
+    };
+    var _addData = function(key, arr){
+        saved_list[key] = saved_list[key].concat(arr);
+    };
+
+    var _removeData = function(key, index){
+        data_public[key].splice(index, 1);
+    };
+
+    return {
+        get_data		: _getData,
+        set_data		: _setData,
+        add_data	    : _addData,
+        remove_data     : _removeData
+    };
+}();
+
+
+var started_position = {
+    x : 0,
+    y : 0
+};
+
+var story = '0';
+var written_text = '';
+var distance = 0;
+var cloud_position_y;
+
+//todo 완료되면 저장하고 목록 불러오기 만들기
+var $running_object;
+var $container = $('#container');
+var $girl = $('#girl');
+var $cloud = $('#cloud');
+var $background_img = $('#background_img');
+var $run_button = $('#run_button');
+/*var $save_button = $('#save_button');*/
+var $refresh_button = $('#refresh_button');
+
+var selected_easing;
+var selected_speed;
+
+var block_width_count = 12;
+var block_height_count = 6;
+
+var background_size = {
+    current_width : 0,
+    current_height : 0,
+    before_width : 0,
+    before_height :0
+};
+
+var block_width_size;
+var block_height_size;
+var running_object_top_fix = $background_img.offset().top;
+var running_object_left_position;
+var running_object_top_position;
+var is_background_img_left_over;
+var is_background_img_top_over;
+
+//todo change image
+/*var d = new Date
+$('#imgg').attr("src", "/fish.png?"+d.getTime());*/
+
+$(function(){
+    $running_object = $girl;
+    story = '0';
+
+    background_size.before_width = $background_img.width();
+    background_size.before_height = $background_img.height();
+    background_size.current_width = $background_img.width();
+    background_size.current_height = $background_img.height();
+
+    $(window).resize(function(){
+        set_position_resize();
+        // onresize();
+    }).resize();
+
+    $run_button.click(function(){
+        set_init();
+        runCode();
+    });
+
+    $refresh_button.click(function() {
+        location.reload();
+    });
+
+    $running_object.draggable({ cursor: "move", containment: $container, scroll: false});
+
+
+    $container.droppable({
+        drop: function( event, ui ) {
+            started_position.x = $running_object.offset().left;
+            started_position.y = $running_object.offset().top - running_object_top_fix;
+        }
+    });
+
+});
+
+function set_init() {
+    $girl.css({"left": started_position.x, "top": started_position.y });
+    $cloud.css({"left": background_size.current_width*0.20, "top": background_size.current_height*0.20 });
+}
+
+function set_position_resize() {
+    background_size.before_width = background_size.current_width;
+    background_size.before_height = background_size.current_height;
+
+    background_size.current_width = $background_img.width();
+    background_size.current_height = $background_img.height();
+
+    block_width_size = (background_size.current_width-$running_object.width()) / (block_width_count);
+    block_height_size = (background_size.current_height-$running_object.height()) / (block_height_count);
+
+    started_position.x = background_size.current_width*started_position.x/background_size.before_width;
+    started_position.y = background_size.current_height*started_position.y/background_size.before_height;
+    $running_object.css({"left": started_position.x, "top": started_position.y });
+    /*cloud_position_y = background_size.current_width*0.20;*/
+    $cloud.css({"left": background_size.current_width*0.20, "top": background_size.current_height*0.20 });
+}
+
+function check_running_object_position(object_running) {
+
+    running_object_left_position = object_running.offset().left;
+    running_object_top_position = object_running.offset().top - running_object_top_fix;
+
+    is_background_img_left_over = ($background_img.width() < running_object_left_position + object_running.width()) || (running_object_left_position < 0);
+    is_background_img_top_over = ($background_img.height() < running_object_top_position + object_running.height()) || (running_object_top_position < 0)
+
+    if(is_background_img_left_over || is_background_img_top_over) {
+        alert('이미지가 넘어갑니다.');
+        running_stop();
+    }
+}
+
+function running_stop() {
+    $cloud.stop(true,false);
+    $girl.stop(true,false);
+
+    set_init();
+}
+
+function toRadians (angle) {
+    return angle * (Math.PI / 180);
+}
