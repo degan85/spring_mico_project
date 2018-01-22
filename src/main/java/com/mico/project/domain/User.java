ccf83752dab93d36e6a19f1c9d5da618cfcb9af0
cho <degan8535@gmail.com>
2018-01-22 17:59:46 +0900

    Added lombok

--- src/main/java/com/mico/project/domain/User.java
+++ src/main/java/com/mico/project/domain/User.java
@@ -13,14 +13,17 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "user")
 @Data
+@Getter @Setter @NoArgsConstructor
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
- 
   private Long id;
   private String username;
   private String password;
@@ -30,38 +33,5 @@ public class User {
              joinColumns = @JoinColumn(name = "user_id"),
              inverseJoinColumns = @JoinColumn(name = "role_id"))
   private Set<Role> roles;
-
-public Long getId() {
-	return id;
-}
-
-public void setId(Long id) {
-	this.id = id;
-}
-
-public String getUsername() {
-	return username;
-}
-
-public void setUsername(String username) {
-	this.username = username;
-}
-
-public String getPassword() {
-	return password;
-}
-
-public void setPassword(String password) {
-	this.password = password;
-}
-
-public Set<Role> getRoles() {
-	return roles;
-}
-
-public void setRoles(Set<Role> roles) {
-	this.roles = roles;
-}
-  
   
 }
